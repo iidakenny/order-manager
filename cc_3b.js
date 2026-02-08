c3e08593ff46f57e920c12b39758cac7097cceac
Iida Kenny <iida@usf.edu>
2026-02-08 14:32:20 -0500

    Updated the price for laptops after 15% discount and updated the inventory after the sale of 5 Smart Watches.

--- cc_3b.js
+++ cc_3b.js
@@ -50,11 +50,18 @@ console.log("Removed Item: ", removedItem);
 
 console.log("\n")
 
+// Creating a sales dicount variable for a 15% discount.
+var salesDiscount = 0.15;
+
+// Updated the price of the laptop to reflect the 15% discount. 
+inventory[1].price = (inventory[1].price * (1 - salesDiscount)).toFixed(2)  ;
+
+// Updated the stock quantity of the smart watch to reflect a sale of 5 units.
+inventory[2].stock -= 5;
+
+// Displayed the updated inventory in a readable summary with SKU, product name, price, and stock quantity.
 inventory.forEach(item => {
     console.log(`SKU-${item.SKU} | ${item.name} | $${item.price} | ${item.stock}`)
 });
-
-// Creating a sales dicount variable for a 15% discount
-var salesDiscount = 0.15;
 
 
