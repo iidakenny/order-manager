697219ce8acfb3e151887eccb3df36421898813d
Iida Kenny <iida@usf.edu>
2026-02-08 14:05:38 -0500

    Created a readable summary for each item in the inventory with SKU, product name, price, and inventory quantity.

--- cc_3b.js
+++ cc_3b.js
@@ -10,18 +10,26 @@ let inventory = [
     {
         SKU: "F890766",
         name: "Laptop",
-        price: 9.99 ,
+        price: 899.99 ,
         stock: 50
     },
     {
         SKU: "F890767",
-        name: "Spoon set, small",
-        price: 19.99 ,stock: 30
+        name: "Smart Watch",
+        price: 299.99,
+        stock: 30
     },
     {
         SKU: "F890768",
-        name: "",
-        price: 15.99 ,
+        name: "Tablet",
+        price: 159.99,
         stock: 20
     }
 ]
+
+// Displayed the inventory in a readable summry with SKU, product name, price, and stock quantity.
+
+inventory.forEach(item => {
+    console.log(`SKU-${item.SKU} | ${item.name} | $${item.price} | ${item.stock}`)
+});
+
