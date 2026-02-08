c92dc029577e0bd4047cd0cf031096fa6772bf73
Iida Kenny <iida@usf.edu>
2026-02-08 13:58:59 -0500

    Initial files and creation of the inventory.

---
+++ cc_3b.js
@@ -0,0 +1,27 @@
+// Created the initial invenory for an electronics store. Each item has a SKU, name, price, and stock quantity.
+
+let inventory = [
+    {
+        SKU: "F890765",
+        name: "Smart Phone", 
+        price: 599.99 ,
+        stock: 70
+    },
+    {
+        SKU: "F890766",
+        name: "Laptop",
+        price: 9.99 ,
+        stock: 50
+    },
+    {
+        SKU: "F890767",
+        name: "Spoon set, small",
+        price: 19.99 ,stock: 30
+    },
+    {
+        SKU: "F890768",
+        name: "",
+        price: 15.99 ,
+        stock: 20
+    }
+]
